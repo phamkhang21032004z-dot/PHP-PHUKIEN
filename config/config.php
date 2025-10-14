0275fe32c1e7bc4e11df74a3a795afb6efe3a323
phamkhang21032004z-dot <phamkhang21032004z@gmail.com>
2025-10-14 23:29:59 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,5 +2,5 @@
 define("DB_HOST", 'hktpc.mysql.database.azure.com');
 define("DB_USER", 'hktpc_khang');
 define("DB_PASS", 'tdc@12345');
-define("DB_NAME", 'hkt_shop1');
+define("DB_NAME", 'hkt_shop');
 define("DB_PORT", '3306');
