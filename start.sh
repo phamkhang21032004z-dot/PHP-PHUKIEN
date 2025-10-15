90579a2947165a0885391a2da263c5bfdfbba88e
phamkhang21032004z-dot <phamkhang21032004z@gmail.com>
2025-10-15 10:32:32 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Cập nhật newrelic.ini nếu env vars thay đổi
-sed -i "s/newrelic.appname=.*/newrelic.appname=\"${NEW_RELIC_APP_NAME:-PHUKIENweb}\"/" /usr/local/etc/php/conf.d/newrelic.ini
+sed -i "s/newrelic.appname=.*/newrelic.appname=\"${NEW_RELIC_APP_NAME:-PHPweb_PHUKIEN}\"/" /usr/local/etc/php/conf.d/newrelic.ini
 sed -i "s/newrelic.license=.*/newrelic.license=\"${NEW_RELIC_LICENSE_KEY}\"/" /usr/local/etc/php/conf.d/newrelic.ini
 service nginx start
 php-fpm
