3219404fd662e3127b701c91bedec50000bf3f4a
phamkhang21032004z-dot <phamkhang21032004z@gmail.com>
2025-10-15 10:38:27 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i "s/newrelic.appname=.*/newrelic.appname=\"${NEW_RELIC_APP_NAME:-PHPweb_PHUKIEN}\"/" /usr/local/etc/php/conf.d/newrelic.ini
+sed -i "s/newrelic.appname=.*/newrelic.appname=\"${NEW_RELIC_APPNAME:-PHPweb_PHUKIEN}\"/" /usr/local/etc/php/conf.d/newrelic.ini
 sed -i "s/newrelic.license=.*/newrelic.license=\"${NEW_RELIC_LICENSE_KEY}\"/" /usr/local/etc/php/conf.d/newrelic.ini
 service nginx start
 php-fpm
